25fac6b601009e1beecabd846f99cc30506f9f4f
John Traut <jtraut1996@gmail.com>
2019-09-13 15:32:31 -0700

    comments

---
+++ traintime-logic.js
@@ -0,0 +1,165 @@
+// Steps to complete:
+
+// 1. Create Firebase link
+// 2. Create initial train data in database
+// 3. Create button for adding new trains - then update the html + update the database
+// 4. Create a way to retrieve trains from the trainlist.
+// 5. Create a way to calculate the time way. Using difference between start and current time.
+//    Then take the difference and modulus by frequency. (This step can be completed in either 3 or 4)
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyCcPFcbAjIsgXGQwE-A3AcOXkeD40qypE8",
+  authDomain: "train-times-93583.firebaseapp.com",
+  databaseURL: "https://train-times-93583.firebaseio.com",
+  storageBucket: "train-times-93583.appspot.com"
+};
+
+firebase.initializeApp(config);
+
+//initilzing a starting point var for our database to grab onto.
+var trainData = firebase.database();
+
+// 2. Populate Firebase Database with initial data (in this case, I did this via Firebase GUI)
+// 3. Button for adding trains
+$("#add-train-btn").on("click", function(event) {
+  // Prevent the default form submit behavior
+  event.preventDefault();
+
+  // Grabs user input
+  //grabs the train name value from the train-name input box
+  var trainName = $("#train-name-input")
+    .val()
+    .trim();
+    //grabs the destination value from the destination input box
+  var destination = $("#destination-input")
+    .val()
+    .trim();
+    //grabs the first train time value first-train input box
+  var firstTrain = $("#first-train-input")
+    .val()
+    .trim();
+
+    //grabs the train frequency from the frequency input box
+  var frequency = $("#frequency-input")
+    .val()
+    .trim();
+
+  // Creates local "temporary" object for holding train data
+
+  var newTrain = {
+//pulling from the variable trainName 
+    name: trainName,
+//pulling from the var destination
+    destination: destination,
+//pulling from var firsTrain
+    firstTrain: firstTrain,
+//pulling from the var frequency
+    frequency: frequency
+  };
+
+  // Uploads train data to the database
+  trainData.ref().push(newTrain);
+
+  // Logs everything to console
+  console.log(newTrain.name);
+  console.log(newTrain.destination);
+  console.log(newTrain.firstTrain);
+  console.log(newTrain.frequency);
+
+  // Alert
+  alert("Train successfully added");
+
+  // Clears all of the text-boxes
+  //this is to allow us to enter in new values without the pervious ones remaining in the text feild
+  $("#train-name-input").val("");
+  $("#destination-input").val("");
+  $("#first-train-input").val("");
+  $("#frequency-input").val("");
+});
+
+// 4. Create Firebase event for adding trains to the database and a row in the html when a user adds an entry
+
+trainData.ref().on("child_added", function(childSnapshot, prevChildKey) {
+  console.log(childSnapshot.val());
+
+  // Store everything into a variable.
+  var tName = childSnapshot.val().name;
+  var tDestination = childSnapshot.val().destination;
+  var tFrequency = childSnapshot.val().frequency;
+  var tFirstTrain = childSnapshot.val().firstTrain;
+
+//creating a new variable called timeArr that stores the value of first train, but split it to present in a more readable manner
+  var timeArr = tFirstTrain.split(":");
+ //this is further taking the timeand converting it to non military time 
+  var trainTime = moment()
+    .hours(timeArr[0])
+    .minutes(timeArr[1]);
+  //setting the max time to equal the train time made above
+  var maxMoment = moment.max(moment(), trainTime);
+  //initilizing new variables to use later
+  var tMinutes;
+  var tArrival;
+
+  // If the first train is later than the current time, sent arrival to the first train time
+  if (maxMoment === trainTime) {
+    tArrival = trainTime.format("hh:mm A");
+    tMinutes = trainTime.diff(moment(), "minutes");
+  } else {
+    // Calculate the minutes until arrival using hardcore math
+    // To calculate the minutes till arrival, take the current time in unix subtract the FirstTrain time
+    // and find the modulus between the difference and the frequency.
+    var differenceTimes = moment().diff(trainTime, "minutes");
+    var tRemainder = differenceTimes % tFrequency;
+    tMinutes = tFrequency - tRemainder;
+    // To calculate the arrival time, add the tMinutes to the current time
+    tArrival = moment()
+      .add(tMinutes, "m")
+      .format("hh:mm A");
+  }
+  console.log("tMinutes:", tMinutes);
+  console.log("tArrival:", tArrival);
+
+  // Add each train's data into the table
+  $("#train-table > tbody").append(
+    $("<tr>").append(
+      $("<td>").text(tName),
+      $("<td>").text(tDestination),
+      $("<td>").text(tFrequency),
+      $("<td>").text(tArrival),
+      $("<td>").text(tMinutes)
+    )
+  );
+});
+
+// Assume the following situations.
+
+// (TEST 1)
+// First Train of the Day is 3:00 AM
+// Assume Train comes every 3 minutes.
+// Assume the current time is 3:16 AM....
+// What time would the next train be...? ( Let's use our brains first)
+// It would be 3:18 -- 2 minutes away
+
+// (TEST 2)
+// First Train of the Day is 3:00 AM
+// Assume Train comes every 7 minutes.
+// Assume the current time is 3:16 AM....
+// What time would the next train be...? (Let's use our brains first)
+// It would be 3:21 -- 5 minutes away
+
+// ==========================================================
+
+// Solved Mathematically
+// Test case 1:
+// 16 - 00 = 16
+// 16 % 3 = 1 (Modulus is the remainder)
+// 3 - 1 = 2 minutes away
+// 2 + 3:16 = 3:18
+
+// Solved Mathematically
+// Test case 2:
+// 16 - 00 = 16
+// 16 % 7 = 2 (Modulus is the remainder)
+// 7 - 2 = 5 minutes away
+// 5 + 3:16 = 3:21
